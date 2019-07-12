3058192acd85a7dd50c9d1b38736614097a377f1
Carlos Noronha <beto_noronha@live.com>
2019-07-12 17:40:21 -0300

    fix sintaxe service layer

--- bdc-portal/src/app/pages/explore/map/layers/layer.service.ts
+++ bdc-portal/src/app/pages/explore/map/layers/layer.service.ts
@@ -1,8 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
  
-import { GeoJsonObject } from 'geojson';
- 
 @Injectable({ providedIn: 'root' })
 export class LayerService {
  
@@ -10,10 +8,10 @@ export class LayerService {
     
     constructor(private http: HttpClient) { }
  
-    /** GET heroes from the server */
-    getLayerGeoJson (ds: string, title: string): any {
+    public async getLayerGeoJson (ds: string, title: string): Promise<Object> {
         let url = `${this.geoserverUrl.replace('<ds>', ds)}?service=WFS&version=1.0.0&request=GetFeature&typeName=${ds}:${title}&&outputFormat=application%2Fjson`;
-        return this.http.get(url).toPromise().then((response: any) => response);
+        const response = await this.http.get(url).toPromise();
+        return response;
     }
 
 }
