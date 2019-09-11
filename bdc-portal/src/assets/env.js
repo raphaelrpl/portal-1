479f26ddf136d29757cabcf9f1cb6ef08f49dc1e
Beto Noronha <beto_noronha@live.com>
2019-09-11 15:01:22 -0300

    change logo

--- bdc-portal/src/assets/env.js
+++ bdc-portal/src/assets/env.js
@@ -4,17 +4,17 @@
     // API url Geoserver
     window.__env.urlGeoserver = 'http://brazildatacube.dpi.inpe.br/geoserver';
     // API url STAC
-    window.__env.urlStac = 'http://localhost:5051';
+    window.__env.urlStac = 'http://cbers1.dpi.inpe.br:5051';
     // API url STAC-Compose
-    window.__env.urlStacCompose = 'http://localhost:5077/stac_compose';
+    window.__env.urlStacCompose = 'http://cbers1.dpi.inpe.br:5077/stac_compose';
     // API url WTSS
-    window.__env.urlWTSS = 'http://localhost:5073/wtss';
+    window.__env.urlWTSS = 'http://cbers1.dpi.inpe.br:5073/wtss';
     // API url Oauth
     window.__env.urlOauth = 'http://brazildatacube.dpi.inpe.br/oauth';
     // API url Soloist
-    window.__env.urlSoloist = 'http://localhost:5021';
+    window.__env.urlSoloist = 'http://cbers1.dpi.inpe.br:5021';
     // API url Maestro
-    window.__env.urlMaestro = 'http://localhost:5020';
+    window.__env.urlMaestro = 'http://cbers1.dpi.inpe.br:5020';
     // API url DataSearch - INPE
     window.__env.urlDataSearchINPE = 'http://www.dpi.inpe.br/datasearch/';
 
